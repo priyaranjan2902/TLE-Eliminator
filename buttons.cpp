d98f5ecec0ec39733504fa61e80005fbeb948702
priyaranjan2902 <rajprience2902@gmail.com>
2026-01-02 21:23:00 +0530

    1858A - Buttons

---
+++ buttons.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int t;
+   cin >> t;
+   while (t--)
+   {
+      int a, b, c;
+      cin >> a >> b >> c;
+      if (a > b)
+         cout << "First\n";
+      else if (b > a)
+         cout << "Second\n";
+      else
+      {
+         if (c % 2)
+            cout << "First\n";
+         else
+            cout << "Second\n";
+      }
+   }
+}
