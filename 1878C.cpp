02425145c8228a06591374fdd1a65adfe0af98a6
priyaranjan2902 <rajprience2902@gmail.com>
2026-01-18 18:27:34 +0530

    1878C - Vasilije in Cacak

---
+++ 1878C.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int t;
+   cin >> t;
+   while (t--)
+   {
+      long long n, k, x;
+      cin >> n >> k >> x;
+      long long mini = (k * (k + 1)) / 2;
+      long long maxi = (n * (n + 1)) / 2;
+      long long diff = n - k;
+      long long diffi = (diff * (diff + 1)) / 2;
+      maxi -= diffi;
+      if (x >= mini && x <= maxi)
+      {
+         cout << "YES" << endl;
+      }
+      else
+      {
+         cout << "NO" << endl;
+      }
+   }
+}
